00b6b1a2adf3ae6da6845d4d613d296dadda55c9
andela-jmwalo <judith.achieng@andela.com>
2017-01-16 19:21:58 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     # If user passed the settings flag ignore the default wger settings
     if not any('--settings' in s for s in sys.argv):
-        setup_django_environment()
+        setup_django_environment(None)
 
     # Alternative to above
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
