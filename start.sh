298117a8eadf3f2168023946aa969ce0c4badf11
alex mwaleh <alex.mwaleh@andela.com>
2017-01-17 16:21:12 +0300

    merge conflict

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 
  invoke create_settings  --settings-path ./wger/settings.py  --database-path ./wger/database.sqlite
  invoke bootstrap_wger --settings-path ./wger/settings.py --no-start-server
+ python manage.py runserver
